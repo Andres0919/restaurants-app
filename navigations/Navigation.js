8c45ee01c9c6437e82957ec9372c81a9f4f467f1
Andrés Posada <andres.posada0919@gmail.com>
2021-03-01 17:44:57 -0500

    verify user account

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
 
 import RestaurantsStack from "./RestaurantsStack";
 import FavoritesStack from "./FavoritesStack";
@@ -54,9 +55,37 @@ const tabs = [
 ];
 
 const Navigation = () => {
+  const screenOptions = (route, color) => {
+    const routeIcon = {
+      restaurants: "compass-outline",
+      favorites: "heart-outline",
+      "top-restaurants": "star-outline",
+      search: "magnify",
+      account: "home-outline",
+    };
+
+    return (
+      <Icon
+        type="material-community"
+        name={routeIcon[route.name]}
+        size={22}
+        color={color}
+      />
+    );
+  };
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurants"
+        tabBarOptions={{
+          activeTintColor: "#442484",
+          inactiveTintColor: "#a17dc3",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
         {tabs.map((tab) => (
           <Tab.Screen
             key={tab.id}
